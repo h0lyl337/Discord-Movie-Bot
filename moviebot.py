445d57c711e1ad6d8febf24e501a6a1ac90bb979
H0lyL337 <deadmanwalkingx13@gmail.com>
2022-05-03 12:21:39 -0400

    Update moviebot.py

--- moviebot.py
+++ moviebot.py
@@ -8,8 +8,7 @@ premusers = ['239357844573388800']
 tvlist = []
 
 bot = Bot(command_prefix='$')
-#insert your token here
-TOKEN = 'TOKEN HERE'
+TOKEN = '< DISCORD BOT TOKEN HERE >'
 
 def get_movie_token():
 	f = open('keys', 'r+')
@@ -26,9 +25,11 @@ def make_token():
 	print(n)
 	return n
 
-def get_movie_list():	
+def get_movie_list():
+	i = 0
 	for movie in os.listdir('/mnt/4f510889-32ca-4946-8d2c-09e9570d7cff/discordbot/static/Movies'):
-		movielist.append(movie)
+		i+=1
+		movielist.append('\n({0}) '.format(i) + movie)
 
 def get_tv_list():	
 	for tv in os.listdir('/mnt/4f510889-32ca-4946-8d2c-09e9570d7cff/discordbot/static/TV'):
@@ -43,32 +44,16 @@ async def on_message(message):
 	if message.content == '$test':
 		await message.channel.send('Testing 1 2 3!')
 
-	if message.content == '$help':
-		embed = discord.Embed(title="A wild pokemon appeared", description="capture the pokemon before it leaves.",color=int(0xFF0000))
-		embed.set_image(url='https://projectpokemon.org/images/normal-sprite/bulbasaur.gif')
-		embed.add_field(name="Bulbasaur", value="Grass/Normal")
-		embed.add_field(name="Lvl:", value="99")
-		embed.add_field(name="HP:", value="100/100")
-		embed.add_field(name="Skills:", value="""razor leaf\n vine whip\n None\n None""")
-		embed.add_field(name="Held Item:", value="Premium Stone")
-		
-		await message.channel.send(embed=embed)
-		userid = message.author.id
-		user = message.author.name
-		await message.author.send('''command list: \n
-			$getmovielist\n
-			$gettvlist\n
-			$getmovie "movie number here"\n
-			$gettv "tv number here"\n
-			$help\n
-			''')
-		print(userid)
 
 	if message.content == '$getmovielist':
 		i = 0
-		for movie in movielist:
-			await message.author.send('{0}) {1}'.format(i, movie))
-			i+=1
+		print(movielist)
+		print(len(movielist))
+		print(round(len(movielist)/10))
+		
+		embed = discord.Embed(title="`A wild pokemon appeared", description='```<br> diff -{0}```'.format(movielist[:13]))	
+		await message.channel.send(embed=embed)
+		await message.author.send("```diff -asdf ```")
 
 	if message.content == '$gettvlist':
 		i = 0
